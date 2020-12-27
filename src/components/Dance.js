2324ae05270d2aa94a1fd6187cff881678b912fe
Leon131314 <lzbikowski23@gmail.com>
2020-12-27 21:07:24 +0100

    added

--- src/components/Dance.js
+++ src/components/Dance.js
@@ -5,7 +5,8 @@ export default class Dance extends React.Component {
     render() {
         return(
             <div className="Dance">
-                    aaaaaaaaaaaaaaaaaaaa
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/VRHWv1UgwxQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/39IXXNnFoC0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         )
     }
