7c7c554b3bf5c9cfffceb5e64c7b1bfa1cfc22fd
Leon131314 <lzbikowski23@gmail.com>
2020-12-28 19:16:57 +0100

    video ups

--- src/components/Music.js
+++ src/components/Music.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import ReactPlayer from 'react-player'
+
+
+
 
 
 export default class Music extends React.Component {
     render() {
         return(
             <div className="Music">
-                    aaaaaaaaaaaaaaaaaaaa
+     <ReactPlayer
+    url="https://youtu.be/VRHWv1UgwxQ"
+    controls
+    playbackRate = {2}
+    width = "896px"
+    height = "504px"
+/>
+            
             </div>
         )
     }
