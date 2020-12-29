695c114ef37390c22942edf54ab6f8636c0e2530
Leon131314 <lzbikowski23@gmail.com>
2020-12-29 18:28:13 +0100

    dance done

--- src/components/Dance.js
+++ src/components/Dance.js
@@ -11,14 +11,14 @@ export default class Dance extends React.Component {
                     <row-1>
 
                   
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/VRHWv1UgwxQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="760" height="415" src="https://www.youtube.com/embed/VRHWv1UgwxQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </row-1>
 
                     <row-3>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/39IXXNnFoC0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="760" height="415" src="https://www.youtube.com/embed/39IXXNnFoC0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </row-3>
                     <row-2>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/Pxfu5NckC0k" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="760" height="415" src="https://www.youtube.com/embed/Pxfu5NckC0k" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
                     </row-2>
                    
@@ -27,16 +27,22 @@ export default class Dance extends React.Component {
             </div>
 
             <div class="timeline">
-            <div class="container right">
+            <div class="container1 right1">
             <div class="content">
-                <h2>2017</h2>
-                <p>Lorem ipsum..</p>
+                <h2>Apr 2020</h2>
+                <p>DANCE SHOWREEL</p>
+            </div>
+            </div>
+            <div class="container2 right2">
+            <div class="content">
+                <h2>Mar 2020</h2>
+                <p>"Me"-OSHUN/ DC Mamma's/ Marta Jakubowska class</p>
             </div>
             </div>
             <div class="container right">
             <div class="content">
-                <h2>2016</h2>
-                <p>Lorem ipsum..</p>
+                <h2>Apr 2019</h2>
+                <p>MASTERS CHOREO</p>
             </div>
             </div>
             </div>
